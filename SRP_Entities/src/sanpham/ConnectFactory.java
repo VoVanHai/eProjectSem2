1304649b0096831d9cd371ff90f8c7ce43f9dbb6
Vinh Mark <daovinh023@gmail.com>
2016-10-12 19:11:12 +0700

    Upload

---
+++ SRP_Entities/src/sanpham/ConnectFactory.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sanpham;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author VINH MARK
+ */
+public class ConnectFactory {
+    private static ConnectFactory factory = null;
+    private Connection con;
+
+    public ConnectFactory() throws Exception{
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=QL_Ban_Hang";
+        con = DriverManager.getConnection(url,"sa","123456");
+    }
+
+    public synchronized static ConnectFactory getInstance() throws Exception{
+        if (factory == null) {
+            factory = new ConnectFactory();
+        }
+        return factory;
+    }
+
+    public Connection getConnection() {
+        return con;
+    }
+    
+    
+}
---
+++ SRP_Entities/src/sanpham/HinhAnhSPDAO.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sanpham;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author VINH MARK
+ */
+public class HinhAnhSPDAO {
+    private Connection conn;
+
+    public HinhAnhSPDAO() throws Exception{
+        conn = ConnectFactory.getInstance().getConnection();
+    }
+    public ArrayList<HinhAnhSP> getAll() throws Exception {
+        ArrayList<HinhAnhSP> lst = new ArrayList<>();
+        String sql = "select * form HINH_ANH_SP";
+        ResultSet rs = conn.prepareStatement(sql).executeQuery();
+        while (rs.next()) {
+            HinhAnhSP ha = new HinhAnhSP(rs.getInt("msHinh"),rs.getString("tenHinh"),rs.getString("duongdan"));
+                    
+            lst.add(ha);
+        }
+        return lst;
+    }
+    
+    //them hinh anh 
+    public boolean add(HinhAnhSP ha) throws Exception {
+        String sql = "insert into HINH_ANH_SP(?,?,?)";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, ha.getMsHinh());
+        ps.setString(2, ha.getTenHinh());
+        ps.setString(3, ha.getDuongdan());
+        return ps.executeUpdate() > 0;
+    }
+    
+    
+    public boolean delete(String msHinh) throws Exception {
+        String sql = "delete From HINH_ANH_SP where MaSP = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, msHinh);
+        return ps.executeUpdate() > 0;
+    }
+    
+    // update san pham 
+    public boolean update(HinhAnhSP ha)throws Exception{
+        String sql = "Update HINH_ANH_SP set tenHinh=?,duongdan=?" + "where msHinh=?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, ha.getTenHinh());
+        ps.setString(2, ha.getDuongdan());
+        ps.setInt(3, ha.getMsHinh());
+        return ps.executeUpdate()>0;
+    }
+
+    //Tìm sản phẩm 
+    public HinhAnhSP findBySanPham(String msHinh) throws Exception {
+        String sql = "select * form HINH_ANH_SP where msHinh = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, msHinh);
+        ResultSet rs = ps.executeQuery();
+        HinhAnhSP ha = null;
+        if (rs.next()) {
+            ha = new HinhAnhSP(rs.getInt("msHinh"), rs.getString("tenHinh"),rs.getString("duongdan"));
+        }
+        return ha;
+    }
+}
