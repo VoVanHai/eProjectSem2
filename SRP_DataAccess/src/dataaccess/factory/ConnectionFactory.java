c24ab70f2fe2f9122d1b6a8f5abe4b6210cd96ce
VoVanHai <vovanhaiqn@hotmail.com>
2016-10-14 18:15:00 +0700

    factory

---
+++ SRP_DataAccess/src/dataaccess/factory/ConnectionFactory.java
@@ -0,0 +1,28 @@
+package dataaccess.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author VoVanHai - https://vovanhai.wordpress.com/
+ */
+public class ConnectionFactory {
+    private Connection con;
+    private static ConnectionFactory factory;
+    private ConnectionFactory()throws Exception{
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url="jdbc:sqlserver://localhost:1433;databaseName=QL_BAN_HANG";
+        con=DriverManager.getConnection(url,"sa","12345678");
+    }
+ 
+    public synchronized static ConnectionFactory getInstance()throws Exception{
+        if(factory==null)
+            factory=new ConnectionFactory();
+        return factory;
+    }
+    
+    public Connection getConection(){
+        return con;
+    }
+}
