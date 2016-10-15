59d377d5051bdd05a5c3e4e00072fc01fbbc5902
Vinh Mark <daovinh023@gmail.com>
2016-10-15 18:24:22 +0700

    fffff

---
+++ SRP_DataAccess/src/dataaccess/sale/NhomBanHangDAO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataaccess.sale;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author VINH MARK
+ */
+public interface NhomBanHangDAO<T> {
+    boolean add(T dao) throws Exception;
+    boolean remove(T dao) throws Exception;
+    boolean find(T dao) throws Exception;
+    ArrayList<T> getAll() throws Exception;
+}
