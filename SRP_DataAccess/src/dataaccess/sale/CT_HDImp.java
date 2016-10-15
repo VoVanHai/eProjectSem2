8aca1ae2889785d9eb910f1045de1573b1c44388
Trần Tiến Dũng <trantiendung.ceo@gmail.com>
2016-10-15 19:11:46 +0700

    update dataacess

---
+++ SRP_DataAccess/src/dataaccess/sale/CT_HDImp.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataaccess.sale;
+import Sale.CT_HD_BH;
+import java.util.ArrayList;
+import sale.
+/**
+ *
+ * @author Tien-Dung
+ *//**
+ *
+ * @author Tien-Dung
+ */
+public class CT_HDImp implements NhomBanHangDAO<CT_HD_BH>{
+
+    @Override
+    public boolean add(CT_HD_BH dao) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean remove(CT_HD_BH dao) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean find(CT_HD_BH dao) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<CT_HD_BH> getAll() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
