2980572d54e78eca07ea6355d0d76244492f3362
Vinh Mark <daovinh023@gmail.com>
2016-10-15 18:57:24 +0700

    đ

---
+++ SRP_DataAccess/src/dataaccess/sale/BarcodeImp.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataaccess.sale;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author VINH MARK
+ */
+public class BarcodeImp implements NhomBanHangDAO<BarcodeImp>{
+
+    @Override
+    public boolean add(BarcodeImp dao) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean remove(BarcodeImp dao) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean find(BarcodeImp dao) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<BarcodeImp> getAll() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
